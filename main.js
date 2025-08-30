317d5ad1fefcf181f64a57622615d485994c91bd
Sachin Sharma <sachinsharma281998@gmail.com>
2025-08-30 12:08:46 +0530

    Removed Unwanted Code

--- main.js
+++ main.js
@@ -20,18 +20,6 @@ cron.schedule("0 0 0 1 * *", async () => {
 }, {
     timezone: "Asia/Kolkata"
 })
-// app.use((req, res, next) => {
-//   const visit = {
-//     ip: req.ip,
-//     path: req.originalUrl,
-//     userAgent: req.headers['user-agent'],
-//     time: new Date()
-//   };
-//   sm.collection('visits').insertOne(visit);
-//   console.log(`A visit was made to your website:`)
-//   console.log(visit.ip)
-//   next();
-// });
 app.get('/', async (req, res) => {
     const students = await countItem('Students');
     const teachers = await countItem('Teachers');
